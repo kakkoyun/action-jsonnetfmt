9a58c4492de1f7df6332319af92b92cb85945775
Kemal Akkoyun <kakkoyun@gmail.com>
2020-03-27 17:19:55 +0100

    Initialize jsonnetfmt action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+cd "$GITHUB_WORKSPACE" || exit 1
+
+echo $(find . -name 'vendor' -prune -o -name 'jsonnet/vendor' -prune -o -name '*.libsonnet' -print -o -name '*.jsonnet' -print) | \
+    xargs -n 1 -- jsonnetfmt -n 2 --max-blank-lines 2 --string-style s --comment-style s -i
