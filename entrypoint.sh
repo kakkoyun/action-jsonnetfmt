e78d67bae636736734847f97e6c5816b31069a5b
Kemal Akkoyun <kakkoyun@users.noreply.github.com>
2020-03-28 15:23:43 +0100

    Use test flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 cd "$GITHUB_WORKSPACE" || exit 1
 
 echo $(find . -name 'vendor' -prune -o -name 'jsonnet/vendor' -prune -o -name '*.libsonnet' -print -o -name '*.jsonnet' -print) | \
-    xargs -n 1 -- jsonnetfmt -n 2 --max-blank-lines 2 --string-style s --comment-style s -i
+    xargs -n 1 -- jsonnetfmt -n 2 --max-blank-lines 2 --string-style s --comment-style s --test
